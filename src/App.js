7b0adc984daeff730b086517c69383c9e872b2dd
DhineshRavi-Tech <dineshravi797@gmail.com>
2022-06-29 16:01:47 +0530

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.css';
-import DataGridLocal from './DataGridLocal';
+import { generateRows, globalSalesValues } from './generator';
 
 export default function App() {
-  return <DataGridLocal></DataGridLocal>;
+  return <p>Hi</p>;
 }
--- src/DataGridLocal.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-class DataGridLocal extends React.Component {
-  render() {
-    return <h2>Hi, I am a Car!</h2>;
-  }
-}
