060624c0d0fc85cb32556278db014d5cc76dd12e
DhineshRavi-Tech <dineshravi797@gmail.com>
2022-06-30 12:38:09 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,138 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './style.css';
-import {
-  SortingState,
-  EditingState,
-  PagingState,
-  SummaryState,
-  IntegratedPaging,
-  IntegratedSorting,
-  IntegratedSummary,
-} from '@devexpress/dx-react-grid';
+import Paper from '@mui/material/Paper';
 import {
   Grid,
   Table,
   TableHeaderRow,
-  TableEditRow,
-  TableEditColumn,
-  PagingPanel,
-  DragDropProvider,
-  TableColumnReordering,
-  TableFixedColumns,
-  TableSummaryRow,
 } from '@devexpress/dx-react-grid-material-ui';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import TableCell from '@mui/material/TableCell';
 
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import SaveIcon from '@mui/icons-material/Save';
-import CancelIcon from '@mui/icons-material/Cancel';
-import { styled } from '@mui/material/styles';
-
-import { ProgressBarCell } from '../../../theme-sources/material-ui/components/progress-bar-cell';
-import { HighlightedCell } from '../../../theme-sources/material-ui/components/highlighted-cell';
-import { CurrencyTypeProvider } from '../../../theme-sources/material-ui/components/currency-type-provider';
-import { PercentTypeProvider } from '../../../theme-sources/material-ui/components/percent-type-provider';
-
-import { generateRows, globalSalesValues } from './generator';
-
-const PREFIX = 'Demo';
-const classes = {
-  lookupEditCell: `${PREFIX}-lookupEditCell`,
-  dialog: `${PREFIX}-dialog`,
-  inputRoot: `${PREFIX}-inputRoot`,
-  selectMenu: `${PREFIX}-selectMenu`,
-};
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${classes.lookupEditCell}`]: {
-    padding: theme.spacing(1),
-  },
-  [`& .${classes.dialog}`]: {
-    width: 'calc(100% - 16px)',
-  },
-  [`& .${classes.inputRoot}`]: {
-    width: '100%',
-  },
-  [`& .${classes.selectMenu}`]: {
-    position: 'absolute !important',
-  },
-}));
-
-const AddButton = ({ onExecute }) => (
-  <div style={{ textAlign: 'center' }}>
-    <Button color="primary" onClick={onExecute} title="Create new row">
-      New
-    </Button>
-  </div>
-);
-
-const EditButton = ({ onExecute }) => (
-  <IconButton onClick={onExecute} title="Edit row" size="large">
-    <EditIcon />
-  </IconButton>
-);
-
-const DeleteButton = ({ onExecute }) => (
-  <IconButton
-    onClick={() => {
-      // eslint-disable-next-line
-      if (window.confirm('Are you sure you want to delete this row?')) {
-        onExecute();
-      }
-    }}
-    title="Delete row"
-    size="large"
-  >
-    <DeleteIcon />
-  </IconButton>
-);
-
-const CommitButton = ({ onExecute }) => (
-  <IconButton onClick={onExecute} title="Save changes" size="large">
-    <SaveIcon />
-  </IconButton>
-);
-
-const CancelButton = ({ onExecute }) => (
-  <IconButton
-    color="secondary"
-    onClick={onExecute}
-    title="Cancel changes"
-    size="large"
-  >
-    <CancelIcon />
-  </IconButton>
-);
-
-const commandComponents = {
-  add: AddButton,
-  edit: EditButton,
-  delete: DeleteButton,
-  commit: CommitButton,
-  cancel: CancelButton,
-};
-
-const Command = ({ id, onExecute }) => {
-  const CommandButton = commandComponents[id];
-  return (
-    <CommandButton
-      onExecute={onExecute}
-    />
-  );
-};
-
-const availableValues = {
-  product: globalSalesValues.product,
-  region: globalSalesValues.region,
-  customer: globalSalesValues.customer,
-};
+const columns = [
+  { name: 'id', title: 'ID' },
+  { name: 'product', title: 'Product' },
+  { name: 'owner', title: 'Owner' },
+];
+const rows = [
+  { id: 0, product: 'DevExtreme', owner: 'DevExpress' },
+  { id: 1, product: 'DevExtreme Reactive', owner: 'DevExpress' },
+];
 
 export default function App() {
-  return <p>Hi</p>;
+  return (
+    <Paper>
+      <Grid rows={rows} columns={columns}>
+        <Table />
+        <TableHeaderRow />
+      </Grid>
+    </Paper>
+  );
 }
